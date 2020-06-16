cd40f99ebd8211f5161cc0adf9a3f8539905d5da
LewLewonics <lewlewonics@gmail.com>
2020-06-16 05:20:49 -0700

    first

---
+++ convertPNGnJPGtoNum.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Change file to number.png
+#                     .jpg   or any other extension that it original 
+counter=1;
+for file in *
+do
+    filename=($file)
+    filename=$(echo "${filename: -4}")
+    echo "$filename"
+    cp $file $counter$filename
+    ((counter++))
+done
---
+++ convertPNGtoJPG.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Converts all PNG File to a JPG file
+for file in *.png
+do
+    #Filename = file minus the .png
+    filename=${file%.png}
+    #Converts all png files to .jpg while maintaining the same file name
+    echo `convert -verbose $file $filename.jpg`
+done
